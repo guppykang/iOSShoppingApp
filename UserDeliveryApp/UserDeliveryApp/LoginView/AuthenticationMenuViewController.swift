9c8c9738a9a0f69adf97fba1a5804c395d0500cd
Joshua Kang <joshuakang@Joshuas-MBP.attlocal.net>
2018-10-13 00:12:03 -0700

    Created the Authentication menu
    Sends the user to the login page or the sign up page based on User button interaction

--- UserDeliveryApp/UserDeliveryApp/LoginView/AuthenticationMenuViewController.swift
+++ UserDeliveryApp/UserDeliveryApp/LoginView/AuthenticationMenuViewController.swift
@@ -10,13 +10,85 @@ import UIKit
 
 class AuthenticationMenuViewController: UIViewController {
 
+    //Title
+    let titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "CART"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    //login button
+    let loginButton : UIButton = {
+        let button = UIButton(type: .system)
+        
+        button.setTitle("Login", for: .normal)
+        button.backgroundColor = .blue
+        button.setTitleColor(.white, for: .normal)
+        
+        button.addTarget(self, action: #selector(handleLoginButton), for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    @objc func handleLoginButton() {
+        let vc = LoginViewController()
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    //Register Button
+    let registerButton : UIButton = {
+        let button = UIButton(type: .system)
+        
+        button.setTitle("Register", for: .normal)
+        button.backgroundColor = .blue
+        button.setTitleColor(.white, for: .normal)
+        
+        button.addTarget(self, action: #selector(handleRegisterButton), for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    @objc func handleRegisterButton() {
+        let vc = RegisterViewController()
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    
     
     func setupLayout() {
+        view.addSubview(titleLabel)
+        view.addSubview(loginButton)
+        view.addSubview(registerButton)
+        
+        //title Label
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        
+        //login button
+        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 150).isActive = true
+        loginButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -50).isActive = true
+        
+        
+        //register button
+        registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        registerButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 10).isActive = true
+        registerButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -50).isActive = true
         
     }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        
+        view.backgroundColor = .white
+        setupLayout()
         
     }
     
---
+++ UserDeliveryApp/UserDeliveryApp/LoginView/RegisterViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RegisterViewController.swift
+//  UserDeliveryApp
+//
+//  Created by Joshua Kang on 10/13/18.
+//  Copyright © 2018 Joshua Kang. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
