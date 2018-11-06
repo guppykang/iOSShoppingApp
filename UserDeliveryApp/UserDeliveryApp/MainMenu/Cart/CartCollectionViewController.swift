b8efda36220ce0abeeeed73c439462df09e47aac
Joshua Kang <joshuakang@Joshuas-MBP.attlocal.net>
2018-11-05 23:39:31 -0800

    Finished UI for the cart
    Need to implement the handle functions for the minus, plus, and delete.
    Need to segue to checkout page when button is pressed.

--- UserDeliveryApp/UserDeliveryApp/MainMenu/Cart/CartCollectionViewController.swift
+++ UserDeliveryApp/UserDeliveryApp/MainMenu/Cart/CartCollectionViewController.swift
@@ -13,7 +13,119 @@ class CartCollectionViewController: UICollectionViewController, UICollectionView
 
     
     private let cellId = "cellId"
-        var cart : [Item] = []
+    var cart : [Item] = []
+    var quantities : [String] = []
+    
+    
+    let checkoutButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("CHECKOUT", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+        button.setTitleColor(.green, for: .normal)
+        
+        button.addTarget(self, action: #selector(handleCheckout), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
+    @objc func handleCheckout() {
+        print("hi dad ")
+    }
+    
+    let nameLabel : UILabel = {
+        let label = UILabel()
+        
+        label.font = UIFont.systemFont(ofSize: 14)
+        
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    let deleteItemButton : UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(named: "TrashCan")
+        button.setImage(image, for: .normal)
+        
+        button.addTarget(self, action: #selector(handleDelete), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let minusItemButton : UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(named: "Minus")
+        button.setImage(image, for: .normal)
+        
+        button.addTarget(self, action: #selector(handleMinus), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let plusItemButton : UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(named: "Plus")
+        button.setImage(image, for: .normal)
+        
+        button.addTarget(self, action: #selector(handlePlus), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let doneButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Done", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.green, for: .normal)
+        
+        button.addTarget(self, action: #selector(handleDone), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    var bottomControlsStackView : UIStackView!
+    
+    func setupChangeQuantityBottomControl(index : Int) {
+        bottomControlsStackView = UIStackView(arrangedSubviews: [nameLabel, deleteItemButton, minusItemButton, plusItemButton, doneButton])
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        if Int(quantities[index])! > 1 {
+            deleteItemButton.isHidden = true
+        }
+        else {
+            minusItemButton.isHidden = true
+        }
+        
+        nameLabel.text = "            Item: \(String(index + 1))"
+        bottomControlsStackView.distribution = .fillEqually
+        
+        view.addSubview(bottomControlsStackView)
+        bottomControlsStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -85).isActive = true
+        bottomControlsStackView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        bottomControlsStackView.heightAnchor.constraint(equalToConstant: 85).isActive = true
+        
+        
+    }
+    @objc func handleDone() {
+        bottomControlsStackView.isHidden = true
+        checkoutButton.isHidden = false
+    }
+    
+    @objc func handlePlus() {
+        print("handle plus")
+    }
+    @objc func handleMinus() {
+        print("handle minus")
+    }
+    
+    @objc func handleDelete() {
+        print("handle delete")
+    }
+    
+    
     
     func getCartItems() {
         Database.database().reference().child("Users").child((Auth.auth().currentUser?.uid)!).child("CurrentOrder").observeSingleEvent(of: .value) { (snapshot) in
@@ -32,8 +144,11 @@ class CartCollectionViewController: UICollectionViewController, UICollectionView
                         let itemSnapshot = item.childSnapshot(forPath: "Item")
                         let itemPath = (itemSnapshot.value as? String)!
                         let itemReference = Database.database().reference(fromURL: itemPath)
-                        print(itemReference)
                         
+                        let quantitySnapshot = item.childSnapshot(forPath: "Quantity")
+                        let quantityInteger = (quantitySnapshot.value as? Int)!
+                        let quantityOfItem  = String(quantityInteger)
+                        self.quantities.append(quantityOfItem)
                         
                         itemReference.observeSingleEvent(of: .value, with: { currentItem in
                             if let dictionary = currentItem.value as? [String: AnyObject] {
@@ -41,22 +156,31 @@ class CartCollectionViewController: UICollectionViewController, UICollectionView
                                 let itemToAdd = Item(dictionary: dictionary)
                                 itemToAdd.path = itemPath
                                 itemToAdd.serialNumber = (currentItem.key as? String)!
-                                self.cart.append(itemToAdd)
+                                
+                                DispatchQueue.main.async(execute: {
+                                    self.cart.append(itemToAdd)
+
+                                    self.collectionView.reloadData()
+                                    
+                                })
                             }
                             
-                            DispatchQueue.main.async(execute: {
-                                self.collectionView.reloadData()
-                                
-                            })
+                            
                         })
                     }
                 }
             })
             
-            print("Done")
-            
-            
         }
+    }
+    
+    func setupViews(){
+        view.addSubview(checkoutButton)
+        
+        checkoutButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -85).isActive = true
+        checkoutButton.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        checkoutButton.heightAnchor.constraint(equalToConstant: 85).isActive = true
+        
     }
     
     
@@ -66,6 +190,9 @@ class CartCollectionViewController: UICollectionViewController, UICollectionView
         
         getCartItems()
         
+        setupViews()
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
         collectionView.register(ItemCell.self, forCellWithReuseIdentifier: cellId)
     }
@@ -76,12 +203,22 @@ class CartCollectionViewController: UICollectionViewController, UICollectionView
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ItemCell
+        print(cart[indexPath.item].name)
         cell.nameLabel.text = cart[indexPath.item].name
+        cell.imageView.loadImageUsingCacheWithUrlString(cart[indexPath.item].imageURL!)
+        cell.quantityLabel.text = "Quantity : \(quantities[indexPath.item])"
+        cell.priceLabel.text = cart[indexPath.item].price
+        cell.indexLabel.text = String(indexPath.item+1)
         return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 225)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        checkoutButton.isHidden = true
+        setupChangeQuantityBottomControl(index: indexPath.item)
     }
 
 }
@@ -101,7 +238,7 @@ class ItemCell: UICollectionViewCell {
         return label
     }()
     
-    let categoryLabel : UILabel = {
+    let quantityLabel : UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 13)
         label.textColor = .darkGray
@@ -113,6 +250,26 @@ class ItemCell: UICollectionViewCell {
         label.font = UIFont.systemFont(ofSize: 13)
         label.textColor = .darkGray
         return label
+    }()	
+    
+    let dividerLineView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        return view
+    }()
+    
+    let indexLabel : UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .darkGray
+        return label
+        
+    }()
+    
+    let arrow : UIImageView = {
+        let image = UIImage(named: "Arrow")
+        let imageView = UIImageView(image: image)
+        return imageView
     }()
     
     
@@ -130,13 +287,47 @@ class ItemCell: UICollectionViewCell {
     }
     
     func setupViews() {
-        backgroundColor = .purple
-
-       // addSubview(imageView)
-        addSubview(nameLabel)
-        //addSubview(categoryLabel)
-        //addSubview(priceLabel)
+        backgroundColor = .white
         
-        nameLabel.frame = CGRect(x: 0, y: frame.width + 2, width: frame.width, height: 40)
+
+        addSubview(imageView)
+        addSubview(nameLabel)
+        addSubview(dividerLineView)
+        addSubview(quantityLabel)
+        addSubview(priceLabel)
+        addSubview(indexLabel)
+        addSubview(arrow)
+        
+        indexLabel.translatesAutoresizingMaskIntoConstraints = false
+        indexLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        indexLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.leftAnchor.constraint(equalTo: indexLabel.rightAnchor, constant: 10).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: self.heightAnchor, constant: 20).isActive = true
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.leftAnchor.constraint(equalTo: imageView.rightAnchor, constant: 10).isActive = true
+        nameLabel.bottomAnchor.constraint(equalTo: priceLabel.topAnchor, constant : -10).isActive = true
+        
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceLabel.leftAnchor.constraint(equalTo: imageView.rightAnchor, constant: 10).isActive = true
+        priceLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        
+        quantityLabel.translatesAutoresizingMaskIntoConstraints = false
+        quantityLabel.leftAnchor.constraint(equalTo: imageView.rightAnchor, constant: 10).isActive = true
+        quantityLabel.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: 10).isActive = true
+
+        arrow.translatesAutoresizingMaskIntoConstraints = false
+        arrow.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -15).isActive = true
+        arrow.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        
+        dividerLineView.translatesAutoresizingMaskIntoConstraints = false
+        dividerLineView.widthAnchor.constraint(equalTo: self.widthAnchor, constant: 10).isActive = true
+        dividerLineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        dividerLineView.topAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+        
     }
 }
